76c9afb18ad5f038d798135f2b6ee3ce785d1f7c
grufre349 <grufre349@gmail.com>
2025-08-19 11:17:05 +0200

    first working version of Context API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { ParentComponent } from "./components/ParentComponent";
+import { createContext } from "react";
+
+export const MyFirstContext = createContext();
 
 const App = () => {
+	const test = "test string for context";
 	return (
 		<>
 			<h1>Context API</h1>
-			<ParentComponent />
+			<MyFirstContext value={test}>
+				<ParentComponent />
+			</MyFirstContext>
 		</>
 	);
 };
--- src/components/GrandChildComponent.jsx
+++ src/components/GrandChildComponent.jsx
@@ -1,7 +1,12 @@
+import { use } from "react";
+import { MyFirstContext } from "../App";
+
 export const GrandChildComponent = () => {
+	const contextVars = use(MyFirstContext);
 	return (
 		<>
 			<h1>Grand Child Component</h1>
+			<p>{contextVars}</p>
 		</>
 	);
 };
